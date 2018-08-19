4610c3d9ef7061ae94d6c98bbcf54be1e7bc2f6d
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-08-19 23:24:06 +0500

    Modified admin registration of student class and used ModelAdmin instead of plain old admin.site.register

--- easyschool/students/admin.py
+++ easyschool/students/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Student
 # Register your models here.
-admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name', 'last_name', 'father_name',
+         'date_of_birth', 'fathers_phone_no'
+        )
+
+    search_fields = (
+        'first_name', 'last_name', 'admission_no'
+        )
+    
+    ordering = ('first_name',)
