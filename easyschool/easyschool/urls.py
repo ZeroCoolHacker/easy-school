2336436930eb9f49e169d0fa5911a9b0c5f32b22
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-05 11:14:19 +0500

    Added Coming soon url to urls

--- easyschool/easyschool/urls.py
+++ easyschool/easyschool/urls.py
@@ -17,8 +17,14 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import render
+
+def coming_soon(request, *args, **kwargs):
+    return render(request, 'soon.html', {})
+
 
 urlpatterns = [
+    url(r'^$', coming_soon),
     url(r'^admin/', admin.site.urls),
     url(r'^api/students/', include('students.api.urls')),
 ]
