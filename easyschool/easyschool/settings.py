e044fef90cca18ede43fd455aba09087dcaae9d9
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-06 17:32:40 +0500

    Added middleware for debug_toolbar

--- easyschool/easyschool/settings.py
+++ easyschool/easyschool/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
