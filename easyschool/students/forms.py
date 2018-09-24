f31345152abceed3846417f6cc912fcf7f753340
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-24 22:24:11 +0500

    Fixed clean method on StudentFeeAdd Form

--- easyschool/students/forms.py
+++ easyschool/students/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import StudentFee
-
+from calendar import month_name
 
 class StudentFeeAdd(forms.ModelForm):
     """ The form class for fee submission """
@@ -10,9 +10,15 @@ class StudentFeeAdd(forms.ModelForm):
         print("clean_month called")
         month = self.cleaned_data['month']
         year = self.cleaned_data['year']
-        qs = StudentFee.objects.filter(month__iexact=month, year__iexact=year)
-        if qs is not None:
-            raise forms.ValidationError("Fee for {} has been submitted".format(month))
+        qs = StudentFee.objects.filter(
+            month__iexact=month,
+             year__iexact=year,
+             student=self.cleaned_data['student'])
+
+
+        if len(qs) is not 0:
+            raise forms.ValidationError("Fee for {} has been submitted".
+            format(month_name[int(month)]))
         return self.cleaned_data
 
     class Meta:
