f05aded9d1c8b24101956ddb87c8dcd8731b386b
oriefy <shahzaibakash@gmail.com>
2019-07-17 11:41:18 +0500

    add str methods to models update django version

--- students/models.py
+++ students/models.py
@@ -60,10 +60,18 @@ class FeeType(models.Model):
     display_name= models.CharField('Displayed Name', max_length=50, blank=False)
     amount      = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
+
+
 class FeeGroup(models.Model):
     name        = models.CharField('Name', max_length=50, blank=False)
     display_name= models.CharField('Displayed Name', max_length=50, blank=False)
     fee_types   = models.ManyToManyField(FeeType,)
+
+    def __str__(self):
+        return self.name
 
 class StudentFee(models.Model):
     """ Datatabase Model for student fees"""
