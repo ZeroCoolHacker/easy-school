8d67727bf72115b033e35f9905d832e641a76738
Shahzaib Akash <shahzaibakash@gmail.com>
2020-10-08 21:51:07 +0500

    remove print statements and unnecessary imports

--- teachers/views.py
+++ teachers/views.py
@@ -1,41 +1,30 @@
 # Create your views here.
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-from django.contrib.auth.views import LoginView as DefaultLoginView
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views import View
 from django.views.generic import CreateView
 
-from teachers import TeacherSignUpForm
+from teachers.forms import TeacherSignUpForm
 
 from .models import Teacher
 
 
-def Login(request):
-    
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        print(username)
-        user=auth.authenticate(username=username,password1=password)
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password1=password)
         if user is not None:
-            auth.login(request,user)
-            print('hello')
+            auth.login(request, user)
             return redirect('home')
-                
         else:
-            messages.error(request,'username or password not correct')
-            print('hey')
+            messages.error(request, 'username or password is not correct')
             return redirect('login')
-
-            
     else:
-        print('hii')
-        return render(request,'login.html') 
+        return render(request, 'login.html')
 
-
-    
 
 class TeacherSignUpFormView(CreateView):
     model = Teacher
@@ -44,7 +33,9 @@ class TeacherSignUpFormView(CreateView):
     success_url = "/"
 
     def form_valid(self, form):
-        c = {'form': form, }
+        context = {
+            'form': form,
+        }
         user = form.save(commit=False)
         # Cleaned(normalized) data
         phone_no = form.cleaned_data['phone_no']
@@ -52,20 +43,30 @@ class TeacherSignUpFormView(CreateView):
         password1 = form.cleaned_data['password1']
         password2 = form.cleaned_data['password2']
         if password1 != password2:
-            messages.error(self.request, "Passwords do not Match", extra_tags='alert alert-danger')
-            return render(self.request, self.template_name, c)
+            messages.error(
+                self.request, "Passwords do not Match", extra_tags='alert alert-danger'
+            )
+            return render(self.request, self.template_name, context)
         user.set_password(password1)
         user.save()
-        date_of_joining= form.cleaned_data['date_of_joining']
+        date_of_joining = form.cleaned_data['date_of_joining']
         cnic = form.cleaned_data['cnic']
-        is_teaching= form.cleaned_data['is_teaching']
-        profile_image=form.cleaned_data['profile_image']
+        is_teaching = form.cleaned_data['is_teaching']
+        profile_image = form.cleaned_data['profile_image']
         address = form.cleaned_data['address']
         gender = form.cleaned_data['gender']
- 
+
         # Create UserProfile model
-        Teacher.objects.create(user=user, phone_no=phone_no, date_of_birth=date_of_birth,date_of_joining=date_of_joining,cnic=cnic,
-                                    is_teaching=is_teaching,profile_image=profile_image,address=address,gender=gender,)
+        Teacher.objects.create(
+            user=user,
+            phone_no=phone_no,
+            date_of_birth=date_of_birth,
+            date_of_joining=date_of_joining,
+            cnic=cnic,
+            is_teaching=is_teaching,
+            profile_image=profile_image,
+            address=address,
+            gender=gender,
+        )
 
         return super(TeacherSignUpFormView, self).form_valid(form)
- 
