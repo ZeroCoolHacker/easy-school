51f85d5f2bb6e034f939f2be44f3e159ca68b098
ali madihi <alimadihib@gmail.com>
2020-10-12 11:46:20 +0330

    feat: add ExportCsvMixin class to export CSV

--- easyschool/utils.py
+++ easyschool/utils.py
@@ -1,4 +1,6 @@
 from datetime import date
+import csv
+from django.http import HttpResponse
 
 GENDER_CHOICES = (
     ('M', 'Male'),
@@ -26,3 +28,21 @@ def next_month():
     year = date.today().year
     day = 1
     return date(year, month, day)
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = 'Export Selected'
