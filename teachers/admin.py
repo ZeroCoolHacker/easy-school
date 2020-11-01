cc5bd52147349a86ec575d182a1fbc160682e02d
marosmola <ssmolenm@gmail.com>
2020-11-01 15:42:09 +0100

    remove migration file and fix unused import

--- teachers/admin.py
+++ teachers/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from django.db.models import Avg, Sum
+from django.db.models import Sum
 from django.db.models.functions import Trunc
 
 from .models import Teacher, TeacherSalary, FinanceSummary
