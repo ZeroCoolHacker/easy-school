8af66eeef9ba4083d8fc63994ed2b9097c0ad46c
aadilrashidd <adilrashid@ymail.com>
2024-04-17 16:59:07 +0500

    update ugettext_lazy as _ to gettext_lazy as _

--- teachers/admin.py
+++ teachers/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.db.models import Sum
 from django.db.models.functions import Trunc
 
